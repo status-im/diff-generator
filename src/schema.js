2c3488c809794def9583ec2079870b3a72787863
Jakub Sokołowski <jakub@status.im>
2019-01-20 17:51:26 +0100

    make commit payload schema more complex
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- src/schema.js
+++ src/schema.js
@@ -1,8 +1,10 @@
 const Joi = require('joi')
  
-const builds = Joi.object().pattern(
-  /^/, Joi.string().uri().allow(null)
-)
+const commit = Joi.object().keys({
+  build_id: Joi.alternatives().try(Joi.number().positive(), Joi.string()).required(),
+  commit: Joi.string().regex(/^[a-zA-Z0-9]{6,40}$/).required(),
+  artifacts: Joi.object().pattern(/^/, Joi.string().uri().allow(null)),
+})
 
 const manual = Joi.object().keys({
   options: Joi.array().items(Joi.string()),
