c82d3a73ed52eaf96abbdb116f5872c5f5d9f1cc
Jakub Sokołowski <jakub@status.im>
2019-01-26 20:34:53 +0100

    add prefixed logging
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- src/server.js
+++ src/server.js
@@ -1,23 +1,27 @@
 const log = require('loglevel')
+const logPrefix = require('@natlibfi/loglevel-message-prefix')
 const Logger = require('koa-logger')
 
 const DB = require('./db')
 const App = require('./app')
 const Diff = require('./diff')
+const DiffoScope = require('./diffoscope')
 
 /* DEFAULTS */
 const LOG_LEVEL        = process.env.LOG_LEVEL        || 'INFO'
 const LISTEN_PORT      = process.env.LISTEN_PORT      || 8000
 const PUBLIC_DOMAIN    = process.env.PUBLIC_DOMAIN    || `http://localhost:${LISTEN_PORT}`
 const DB_PATH          = process.env.DB_PATH          || '/tmp/diffgen.db'
-const DB_SAVE_INTERVAL = process.env.DB_SAVE_INTERVAL || 5000
+const TEMP_PATH        = process.env.DB_PATH          || '/tmp/download'
 const DIFF_OUTPUT_PATH = process.env.DIFF_OUTPUT_PATH || '/tmp/diffs'
 
 /* set the logging level (TRACE, DEBUG, INFO, WARN, ERROR, SILENT) */
 log.setDefaultLevel(log.levels[LOG_LEVEL])
+logPrefix(log, {prefixFormat:'%p:'})
 
-const db = new DB(DB_PATH, DB_SAVE_INTERVAL)
-const diff = new Diff(db, DIFF_OUTPUT_PATH)
+const db = new DB(DB_PATH)
+const dos = new DiffoScope(DIFF_OUTPUT_PATH, TEMP_PATH)
+const diff = new Diff(db, dos)
 const app = App(PUBLIC_DOMAIN, db, diff)
 
 app.use(Logger())
